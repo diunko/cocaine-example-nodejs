c5ef1c5bc58a643b4355a4533bff4a1b95314e70
Dmitry Unkovsky <oil.crayons@gmail.com>
2014-02-06 12:30:58 +0400

    minor corrections

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var app = require('express')()
 var format = require('util').format
 
 app.get('/', function(req, res){
+  console.log('processing / handler')
   var body = format('worker %s\n', process.pid)
   res.setHeader('content-type', 'text/plain')
   res.setHeader('content-length', body.length)
