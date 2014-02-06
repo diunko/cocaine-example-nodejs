b239e39083570435dfefa0d8ccced24ff989f7e1
Dmitry Unkovsky <oil.crayons@gmail.com>
2014-02-06 14:09:51 +0400

    make use of services

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 
-var optimist = require('optimist').argv
+var argv = require('optimist').argv
 var cocaine = require('cocaine')
+
+var locator = argv.locator || 'apefront.tst.ape.yandex.net:10053'
+
+var client = new cocaine.Client(locator)
+
+client.on('error', function(err){
+  console.log('client error', err)
+})
 
 if(cocaine.spawnedBy()){
   var http = cocaine.http
@@ -11,31 +19,55 @@ if(cocaine.spawnedBy()){
 var app = require('express')()
 var format = require('util').format
 
-app.get('/', function(req, res){
-  console.log('processing / handler')
-  var body = format('worker %s\n', process.pid)
-  res.setHeader('content-type', 'text/plain')
-  res.setHeader('content-length', body.length)
-  res.end(body)
-})
+client.getServices(['logging', 'geobase', 'langdetect'], function(err, log, geobase, langdetect){
+  if(err){
+    throw new Error(format('error connecting to one of services: %j', err))
+  }
 
-app.get('/hello', function(req, res){
-  console.log('processing /hello handler')
-  res.send(format('Hi, I\'m no.%s!\n', process.pid))
-})
-
-var server = http.createServer(app)
-var port = process.env.PORT || 5000
-
-if(cocaine.spawnedBy()){
-  var W = new cocaine.Worker(argv)
-  var handle = W.getListenHandle('http')
-  server.listen(handle, function(){
-    console.log('listening on cocaine handle')
+  app.get('/', function(req, res){
+    log.debug('processing / handler')
+    var body = format('worker %s\n', process.pid)
+    res.setHeader('content-type', 'text/plain')
+    res.setHeader('content-length', body.length)
+    res.end(body)
   })
-} else {
-  server.listen(port, function(){
-    console.log('listening on port', port)
+
+  app.get('/hello', function(req, res){
+    log.debug('processing /hello handler')
+    res.send(format('Hi, I\'m no.%s!\n', process.pid))
   })
-}
+
+  app.get('/geo', function(req, res){
+    log.debug('processing /geo handler')
+    var ip = req.headers['x-real-ip'] || '1.2.3.4'
+    geobase.region_id(req.headers['x-real-ip'], function(err, region){
+      if(err){
+        log.debug('error looking up region', err.message)
+        res.send('error looking up region: ' + err.message)
+      } else {
+        log.debug('got region:', region)
+        res.send('got region: '+region)
+      }
+    })
+  })
+
+  var server = http.createServer(app)
+  var port = process.env.PORT || 5000
+
+  if(cocaine.spawnedBy()){
+    var W = new cocaine.Worker(argv)
+    var handle = W.getListenHandle('http')
+    server.listen(handle, function(){
+      log.debug('listening on cocaine handle')
+    })
+    W.events.on('error', function(err){
+      log.debug('cocaine worker error', err.message)
+    })
+  } else {
+    server.listen(port, function(){
+      log.debug('listening on port', port)
+    })
+  }
+
+})
 
