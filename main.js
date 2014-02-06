a6fadc5a1de8d9e4ad961b6c02444cd7587e43a0
Dmitry Unkovsky <oil.crayons@gmail.com>
2014-02-06 13:34:22 +0400

    add basic cocaine support

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 
-var http = require('http')
+var optimist = require('optimist').argv
+var cocaine = require('cocaine')
+
+if(cocaine.spawnedBy()){
+  var http = cocaine.http
+} else {
+  var http = require('http')
+}
 
 var app = require('express')()
 var format = require('util').format
@@ -12,7 +19,6 @@ app.get('/', function(req, res){
   res.end(body)
 })
 
-
 app.get('/hello', function(req, res){
   console.log('processing /hello handler')
   res.send(format('Hi, I\'m no.%s!\n', process.pid))
@@ -20,7 +26,16 @@ app.get('/hello', function(req, res){
 
 var server = http.createServer(app)
 var port = process.env.PORT || 5000
-server.listen(port, function(){
-  console.log('listening on port', port)
-})
+
+if(cocaine.spawnedBy()){
+  var W = new cocaine.Worker(argv)
+  var handle = W.getListenHandle('http')
+  server.listen(handle, function(){
+    console.log('listening on cocaine handle')
+  })
+} else {
+  server.listen(port, function(){
+    console.log('listening on port', port)
+  })
+}
 
